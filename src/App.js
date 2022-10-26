f82605fb9974afacff5da829134c296e4e87592a
nagrzesikowska <94862333+nagrzesikowska@users.noreply.github.com>
2022-10-26 18:21:48 +0200

    added weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from "./logo.svg";
+import React from "react";
+import Temperature from "./Temperature";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class WeatherApp extends React.Component {
+  render = () => {
+    return (
+      <div>
+        <div className="container">
+          <div className="weather-app">
+            <div className="row">
+              <div className="col-4 search-bar">
+                <input
+                  type="text"
+                  className="search-input"
+                  placeholder="type a city.."
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-6 search-button">
+                <input type="submit" value="search" class="button" />
+              </div>
+            </div>
+            <div class="col-4 board-today">
+              <div className="city">KRAKÓW</div>
+              <div className="date">sunday 10/10/21</div>
+              <div className="time">18:45</div>
+              <div className="temperature">
+                <Temperature />
+              </div>
+            </div>
+            <div className="col-4 today-parameters">
+              <div class="humidity">
+                HUMIDITY
+                <br />
+                <p class="value">56%</p>
+              </div>
+              <div class="wind">
+                WIND
+                <br />
+                <p className="value">10km/h</p>
+              </div>
+              <div className="air quality">
+                AIR QUALITY
+                <br />
+                <p className="value">good</p>
+              </div>
+            </div>
 
-export default App;
+            <div className="weather-forecast">
+              <div className="row">
+                <div className="col-2">
+                  <div className="day">Sunday</div>
+                  <div className="forecast-temerature">15/2°C</div>
+                </div>
+                <div className="col-2">
+                  <div className="day">Monday</div>
+                  <div className="forecast-temerature">13/5°C</div>
+                </div>
+                <div className="col-2">
+                  <div className="day">Tuesday</div>
+                  <div className="forecast-temerature">21/12°C</div>
+                </div>
+                <div className="col-2">
+                  <div className="day">Wednesday</div>
+                  <div className="forecast-temerature">10/2°C</div>
+                </div>
+                <div className="col-2">
+                  <div className="day">Thursday</div>
+                  <div className="forecast-temerature">7/2°C</div>
+                </div>
+                <div className="col-2">
+                  <div className="day">Friday</div>
+                  <div className="forecast-temerature">1/-1°C</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <a href="https://github.com/nagrzesikowska/weather-app">
+              {" "}
+              Open-source code
+            </a>
+            <a> by Natalia Grzesikowska</a>
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
---
+++ src/Temperature.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Temperature extends React.Component {
+  render = () => {
+    return <div className="temperature">19°C</div>;
+  };
+}
